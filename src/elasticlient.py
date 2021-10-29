522b4231e64d572d9942704e36b46533664e8cb3
davidcawork <davidcawork@gmail.com>
2021-10-29 07:53:24 +0000

    Updated checks

--- src/elasticlient.py
+++ src/elasticlient.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from elasticsearch import Elasticsearch
-import os
+import os, logging, requests
 
 class ElastiClient(object):
     """
@@ -16,10 +16,38 @@ class ElastiClient(object):
         self.usr_table = usr_table
         self.num_table = num_table
         
-    @staticmethod
-    def checkElasticsearch():
+    def checkElasticsearch(self):
         """
             Método para asegurarse que el servicio está corriendo
         """
+
+        # Si el servicio esta inactivo lo levantamos
         if os.system('systemctl is-active --quiet elasticsearch.service') != 0:
-            os.system('systemctl status elasticsearch.service')
+            logging.debug('Servicio elasticsearch.service inactivo... levantando')
+            os.system('systemctl start elasticsearch.service')
+        else:
+            logging.debug('Servicio elasticsearch.service activo!')
+
+        # Comprobamos que Elasticsearch esté corriendo en el puerto y en el host indicado
+        if requests.get('http://' + self.host + ':' + str(self.port)).text is not None:
+            logging.debug('Elasticsearch corriendo correctamente en ' + self.host + ':' + str(self.port))
+        else:
+            logging.error('No se ha encontrado el servicio de Elasticsearch corriendo en ' + self.host + ':' + str(self.port))
+    
+
+    def initDataTables(self):
+        """
+            Metodo para iniciar correctamente las tablas que se van a utilizar
+        """
+        # Vamos a empezar por la tabla de numeros rng
+        if es.indices.exists(index=self.num_table):
+            logging.debug('Se ha encontrado index ' + self.num_table +' creado... regenerando')
+            es.indices.delete(index=self.num_table)
+            
+
+
+
+    
+
+logging.basicConfig(format='[%(levelname)s] %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
+ElastiClient('localhost', 9200).checkElasticsearch()
